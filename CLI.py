0c16a150f1614d554c354cd5dff22f8bb01f6695
Yushin Jung <jyushin90@gmail.com>
2023-02-13 07:01:42 +0000

    test cli

--- CLI.py
+++ CLI.py
@@ -15,6 +15,7 @@ def CLI():
 @click.argument('subject')
 @click.argument('text_message')
 def send_simple(sender_email, password, receiver_email, subject, text_message):
+    print(sender_email, password, receiver_email, subject, text_message)
     send.send_simpleNotice(sender_email, password, receiver_email, subject, text_message)
     
 if __name__=='__main__':
