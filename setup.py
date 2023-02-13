b6f275767af4e7758fbd2559923a81e1718f09a9
Yushin Jung <jyushin90@gmail.com>
2023-02-13 09:33:04 +0000

    setup.py error fixed

--- setup.py
+++ setup.py
@@ -8,10 +8,8 @@ setup(
     author='Yushin Jung',
     author_email='jyushin90@gmail.com',
     license='GNU General Public License v3.0',
-    packages=['atg-email'], 
     install_requires=[
         'click==8.1.3'
     ]
-    
 )
 
